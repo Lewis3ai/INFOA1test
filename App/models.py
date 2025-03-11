8404c431d386621cc4f68c8766cc97b2274c90b3
Lewis3ai <158581435+Lewis3ai@users.noreply.github.com>
2025-03-11 01:24:07 +0000

    updated fully

--- App/models.py
+++ App/models.py
@@ -1,46 +1,30 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql.expression import func
+from sqlalchemy.sql.functions import user
 from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
 
 
-# Define Pokemon model
-class Pokemon(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120), nullable=False)
-    type = db.Column(db.String(120), nullable=False)
-    description = db.Column(db.String(255), nullable=True)
 
-    def __init__(self, name, type, description=None):
-        self.name = name
-        self.type = type
-        self.description = description
 
-    def get_json(self):
-        return {
-            "id": self.id,
-            "name": self.name,
-            "type": self.type,
-            "description": self.description
-        }
 
-    def __repr__(self):
-        return f'<Pokemon {self.id} - {self.name}>'
-
+    
 
 # Define UserPokemon model for the many-to-many relationship between users and Pokemon
 class UserPokemon(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     pokemon_id = db.Column(db.Integer, db.ForeignKey('pokemon.id'), nullable=False)
-
+    name = db.Column(db.String(50), nullable=False)
+    
     user = db.relationship('User', backref=db.backref('user_pokemon', lazy=True))
     pokemon = db.relationship('Pokemon', backref=db.backref('user_pokemon', lazy=True))
 
-    def __init__(self, user_id, pokemon_id):
+    def __init__(self, user_id, pokemon_id, name):
         self.user_id = user_id
         self.pokemon_id = pokemon_id
+        self.name = name
 
     def get_json(self):
         return {
@@ -53,13 +37,13 @@ class UserPokemon(db.Model):
         return f'<UserPokemon {self.id} - User {self.user_id} with Pokemon {self.pokemon_id}>'
 
 
+
 # Define User model
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
-    type = db.Column(db.String(50))
     __mapper_args__ = {'polymorphic_identity': 'user', 'polymorphic_on': type}
 
     def __init__(self, username, email, password):
@@ -87,5 +71,50 @@ class User(db.Model):
         }
 
 
+# Define Pokemon model
+class Pokemon(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    attack = db.Column(db.Integer, nullable=False)
+    defense = db.Column(db.Integer, nullable=False)
+    hp = db.Column(db.Integer, nullable=False)
+    height = db.Column(db.Float, nullable=True)
+    weight = db.Column(db.Float, nullable=True)
+    sp_attack = db.Column(db.Integer, nullable=False)
+    sp_defense = db.Column(db.Integer, nullable=False)
+    speed = db.Column(db.Integer, nullable=False)
+    type1 = db.Column(db.String(50), nullable=False)
+    type2 = db.Column(db.String(50), nullable=True)  # Nullable for single-type Pokemon
 
+    def __init__(self, id, name, attack, defense, hp, height, weight, sp_attack, sp_defense, speed, type1, type2=None):
+        self.id = id
+        self.name = name
+        self.attack = attack
+        self.defense = defense
+        self.hp = hp
+        self.height = height
+        self.weight = weight
+        self.sp_attack = sp_attack
+        self.sp_defense = sp_defense
+        self.speed = speed
+        self.type1 = type1
+        self.type2 = type2
 
+    def get_json(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "attack": self.attack,
+            "defense": self.defense,
+            "hp": self.hp,
+            "height": self.height,
+            "weight": self.height,
+            "sp_attack": self.sp_attack,
+            "sp_defense": self.sp_defense,
+            "speed": self.speed,
+            "type1": self.type1,
+            "type2": self.type2
+        }
+
+    def __repr__(self):
+        return f'<Pokemon {self.id} - {self.name}>'
