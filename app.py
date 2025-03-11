70b8808c61adb23a58be77cc22fb64c1a616b826
Lewis3ai <158581435+Lewis3ai@users.noreply.github.com>
2025-03-11 00:52:01 +0000

    update

--- app.py
+++ app.py
@@ -1,207 +1,203 @@
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.sql.expression import func
-from werkzeug.security import generate_password_hash, check_password_hash
+import os, csv
+from flask import Flask, jsonify, request
+from functools import wraps
+from flask_cors import CORS
+from sqlalchemy.exc import IntegrityError
+from flask_jwt_extended import (
+    JWTManager,
+    create_access_token,
+    get_jwt_identity,
+    jwt_required,
+    set_access_cookies,
+    unset_jwt_cookies,
+)
 
-db = SQLAlchemy()
+from .models import db, User, UserPokemon, Pokemon
+
+# Configure Flask App
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'MySecretKey'
+app.config['JWT_ACCESS_COOKIE_NAME'] = 'access_token'
+app.config['JWT_REFRESH_COOKIE_NAME'] = 'refresh_token'
+app.config["JWT_TOKEN_LOCATION"] = ["cookies", "headers"]
+app.config["JWT_COOKIE_SECURE"] = True
+app.config["JWT_SECRET_KEY"] = "super-secret"
+app.config["JWT_COOKIE_CSRF_PROTECT"] = False
+# app.config['JWT_HEADER_TYPE'] = ""
+app.config['JWT_HEADER_NAME'] = "Cookie"
 
 
-class Todo(db.Model):
-  id = db.Column(db.Integer, primary_key=True)
-  user_id = db.Column(db.Integer, db.ForeignKey('user.id'),
-                      nullable=False)  #set userid as a foreign key to user.id
-  text = db.Column(db.String(255), nullable=False)
-  done = db.Column(db.Boolean, default=False)
+# Initialize App 
+db.init_app(app)
+app.app_context().push()
+CORS(app)
+jwt = JWTManager(app)
 
-  def toggle(self):
-    self.done = not self.done
-    db.session.add(self)
+# Initializer Function to be used in both init command and /init route
+def initialize_db():
+  db.drop_all()
+  db.create_all()
+
+
+
+# customn decorator authorize routes for admin or regular user
+def login_required(required_class):
+  def wrapper(f):
+    @wraps(f)
+    @jwt_required()  # Ensure JWT authentication
+    def decorated_function(*args, **kwargs):
+      user = required_class.query.filter_by(
+          username=get_jwt_identity()).first()
+      if user.__class__ != required_class:  # Check class equality
+        return jsonify(error='Invalid user role'), 403
+      return f(*args, **kwargs)
+    return decorated_function
+  return wrapper
+
+
+def login_user(username, password):
+  user = User.query.filter_by(username=username).first()
+  if user and user.check_password(password):
+    token = create_access_token(identity=username)
+    response = jsonify(access_token=token)
+    set_access_cookies(response, token)
+    return response
+  return jsonify(error="Invalid username or password"), 401
+
+
+@app.route('/')
+def index():
+  return '<h1>mY Todo API</h1>'
+
+
+# Login
+@app.route('/login', methods=['POST'])
+def user_login_view():
+  data = request.json
+  return login_user(data['username'], data['password'])
+
+
+@app.route('/identify')
+@jwt_required()
+def identify_view():
+  username = get_jwt_identity()
+  user = User.query.filter_by(username=username).first()
+  if user:
+    return jsonify(user.get_json())
+  return jsonify(message='Invalid user'), 403
+
+
+# Sign Up
+@app.route('/signup', methods=['POST'])
+def signup_user_view():
+  data = request.json
+  try:
+    new_user = RegularUser(data['username'], data['email'], data['password'])
+    db.session.add(new_user)
     db.session.commit()
-
-  def __init__(self, text):
-    self.text = text
-
-  def get_cat_list(self):
-    return ', '.join([category.text for category in self.categories])
-
-  def get_json(self):
-    return {
-        "id": self.id,
-        "text": self.text,
-        "done": self.done,
-        "user": self.user.username,
-        "categories": self.get_cat_list()
-    }
-
-  def __repr__(self):
-    return f'<Todo: {self.id} | {self.user.username} | {self.text} | { "done" if self.done else "not done" } | categories [{self.get_cat_list()}]>'
+    return jsonify(
+        message=f'User {new_user.id} - {new_user.username} created!'), 201
+  except IntegrityError:
+    db.session.rollback()
+    return jsonify(error='Username already exists'), 400
 
 
-class TodoCategory(db.Model):
-  __tablename__ = 'todo_category'
-  id = db.Column(db.Integer, primary_key=True)
-  todo_id = db.Column(db.Integer, db.ForeignKey('todo.id'), nullable=False)
-  category_id = db.Column(db.Integer,
-                          db.ForeignKey('category.id'),
-                          nullable=False)
-  last_modified = db.Column(db.DateTime,
-                            default=func.now(),
-                            onupdate=func.now())
-
-  def __repr__(self):
-    return f'<TodoCategory last modified {self.last_modified.strftime("%Y/%m/%d, %H:%M:%S")}>'
+# Logout
+@app.route('/logout', methods=['GET'])
+def logout():
+  response = jsonify(message='Logged out')
+  unset_jwt_cookies(response)
+  return response
 
 
-class Category(db.Model):
-  id = db.Column(db.Integer, primary_key=True)
-  user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-  text = db.Column(db.String(255), nullable=False)
-  user = db.relationship('RegularUser',
-                         backref=db.backref('categories', lazy='joined'))
-  todos = db.relationship('Todo',
-                          secondary='todo_category',
-                          backref=db.backref('categories', lazy=True))
-
-  def __init__(self, user_id, text):
-    self.user_id = user_id
-    self.text = text
-
-  def __repr__(self):
-    return f'<Category user:{self.user.username} - {self.text}>'
+# Todo Crud Operations
 
 
-class User(db.Model):
-  id = db.Column(db.Integer, primary_key=True)
-  username = db.Column(db.String(80), unique=True, nullable=False)
-  email = db.Column(db.String(120), unique=True, nullable=False)
-  password = db.Column(db.String(120), nullable=False)
-  type = db.Column(db.String(50))
-  __mapper_args__ = {'polymorphic_identity': 'user', 'polymorphic_on': type}
-
-  def __init__(self, username, email, password):
-    self.username = username
-    self.email = email
-    self.set_password(password)
-
-  def set_password(self, password):
-    """Create hashed password."""
-    self.password = generate_password_hash(password, method='scrypt')
-
-  def check_password(self, password):
-    """Check hashed password."""
-    return check_password_hash(self.password, password)
-
-  def __repr__(self):
-    return f'<User {self.id} {self.username} - {self.email}>'
-
-  def get_json(self):
-    return {
-        "id": self.id,
-        "username": self.username,
-        "email": self.email,
-        "type": self.type
-    }
+@app.route('/todos', methods=['POST'])
+@login_required(RegularUser)
+def create_todo_view():
+  data = request.json
+  username = get_jwt_identity()
+  user = RegularUser.query.filter_by(username=username).first()
+  new_todo = user.add_todo(data['text'])
+  return jsonify(message=f'todo {new_todo.id} created!'), 201
 
 
-class RegularUser(User):
-  __tablename__ = 'regular_user'
-  todos = db.relationship( 'Todo', backref='user',lazy=True)  # sets up a relationship to todos which references User
-  __mapper_args__ = {
-      'polymorphic_identity': 'regular user',
-  }
-
-  def add_todo(self, text):
-    new_todo = Todo(text=text)
-    new_todo.user_id = self.id
-    self.todos.append(new_todo)
-    db.session.add(self)
-    db.session.commit()
-    return new_todo
-
-  def delete_todo(self, todo_id):
-    todo = Todo.query.filter_by(id=todo_id, user_id=self.id).first()
-    if todo:
-      db.session.delete(todo)
-      db.session.commit()
-      return True
-    return None
-
-  def update_todo(self, todo_id, text):
-    todo = Todo.query.filter_by(id=todo_id, user_id=self.id).first()
-    if todo:
-      todo.text = text
-      db.session.add(todo)
-      db.session.commit()
-      return True
-    return None
-
-  def toggle_todo(self, todo_id):
-    todo = Todo.query.filter_by(id=todo_id, user_id=self.id).first()
-    if todo:
-      todo.toggle()
-      return todo
-    return None
-
-  def add_todo_category(self, todo_id, category_text):
-    category = Category.query.filter_by(text=category_text).first()
-    todo = Todo.query.filter_by(id=todo_id, user_id=self.id).first()
-
-    # todo_id given does not exist or belong to user
-    if not todo:
-      return None
-
-    # create category if it doesn't exist
-    if not category:
-      category = Category(self.id, category_text)
-      db.session.add(category)
-      db.session.commit()
-
-    #check if todo already has the category
-    if category not in todo.categories:
-      category.todos.append(todo)
-      db.session.add(category)
-      db.session.commit()
-    return category
-
-  def getNumTodos(self):
-    return len(self.todos)
-
-  def getDoneTodos(self):
-    numDone = 0
-    for todo in self.todos:
-      if todo.done:
-        numDone += 1
-    return numDone
-
-  def __repr__(self):
-    return f'<RegularUser {self.id} : {self.username} - {self.email}>'
+@app.route('/todos', methods=['GET'])
+@login_required(RegularUser)
+def get_todos_view():
+  # get the user object of the authenticated user
+  user = RegularUser.query.filter_by(username=get_jwt_identity()).first()
+  # converts todo objects to list of todo dictionaries
+  todo_json = [todo.get_json() for todo in user.todos]
+  todo_json.reverse()
+  return jsonify(todo_json), 200
 
 
-class Admin(User):
-  __tablename__ = 'admin'
-  staff_id = db.Column(db.String(120), unique=True)
-  __mapper_args__ = {
-      'polymorphic_identity': 'admin',
-  }
+@app.route('/todos/<int:id>', methods=['GET'])
+@login_required(RegularUser)
+def get_todo_view(id):
+  todo = Todo.query.get(id)
 
-  def get_all_todos_json(self):
-    todos = Todo.query.all()
-    if todos:
-      return [todo.get_json() for todo in todos]
-    else:
-      return []
+  # must check if todo belongs to the authenticated user
+  if not todo or todo.user.username != get_jwt_identity():
+    return jsonify(error="Bad ID or unauthorized"), 401
 
-  def __init__(self, staff_id, username, email, password):
-    super().__init__(username, email, password)
-    self.staff_id = staff_id
+  return jsonify(todo.get_json()), 200
 
-  def get_json(self):
-    return {
-        "id": self.id,
-        "username": self.username,
-        "email": self.email,
-        "staff_id": self.staff_id,
-        "type": self.type
-    }
 
-  def __repr__(self):
-    return f'<Admin {self.id} : {self.username} - {self.email}>'
+@app.route('/todos/<int:id>', methods=['PUT'])
+@login_required(RegularUser)
+def edit_todo_view(id):
+  data = request.json
+  user = RegularUser.query.filter_by(username=get_jwt_identity()).first()
+
+  todo = Todo.query.get(id)
+
+  # must check if todo belongs to the authenticated user
+  if not todo or todo.user.username != get_jwt_identity():
+    return jsonify(error="Bad ID or unauthorized"), 401
+
+  user.update_todo(id, data['text'])
+  return jsonify(message=f"todo updated to '{data['text']}'!"), 200
+
+@app.route('/todos/<int:id>/done', methods=['PUT'])
+@login_required(RegularUser)
+def toggle_todo_view(id):
+  user = RegularUser.query.filter_by(username=get_jwt_identity()).first()
+
+  todo = Todo.query.get(id)
+
+  # must check if todo belongs to the authenticated user
+  if not todo or todo.user.username != get_jwt_identity():
+    return jsonify(error="Bad ID or unauthorized"), 401
+
+  todo = user.toggle_todo(id)
+  return jsonify(message=f"todo {'done' if todo.done else 'not done'}!"), 200
+
+@app.route('/todos/<int:id>', methods=['DELETE'])
+@login_required(RegularUser)
+def delete_todo_view(id):
+  user = RegularUser.query.filter_by(username=get_jwt_identity()).first()
+  todo = Todo.query.get(id)
+
+  # must check if todo belongs to the authenticated user
+  if not todo or todo.user.username != get_jwt_identity():
+    return jsonify(error="Bad ID or unauthorized"), 401
+
+  user.delete_todo(id)
+  return jsonify(message="todo deleted!"), 200
+
+
+@app.route('/todos/stats', methods=['GET'])
+@login_required(RegularUser)
+def get_stats_view():
+  user = RegularUser.query.filter_by(username=get_jwt_identity()).first()
+  return jsonify(num_todos=user.getNumTodos(),
+                 num_done=user.getDoneTodos()), 200
+
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', port=81)
